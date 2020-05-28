4176477d374336dfe2d53aca3f3a941a0a413e18
MiKl <51372994+MiKl-LS@users.noreply.github.com>
2020-05-28 20:11:52 +0800

    Update NtW.js

--- NtW.js
+++ NtW.js
@@ -1,191 +1,121 @@
-// Declare spelling list of numbers
-var onesList = [ "", "one", "two", "three", "four", "five", "six", "seven", "eight", "nine" ]
-var teensList = [ "ten", "eleven", "twelve", "thirteen", "fourteen", "fifteen", "sixteen", 
-	"seventeen", "eighteen", "nineteen", "twenty" ]
-var tensList = [ "", "", "twenty ", "thirty ", "forty ", "fifty ", "sixty ", "seventy ", "eighty ", 
-	"ninety " ]
-// Add a reader the processes numbers and outputs their spelling
-function reader(n, l) {
+// arrays of spellings 
+dgt = [ "", "one", "two", "three", "four", "five", "six", "seven", "eight", "nine", "ten" ]
+tns = ["ten", "eleven", "twelve", "thirteen", "fourteen", "fifteen", "sixteen", "seventeen", "eighteen", "nineteen", "twenty" ]
+tens = [ "", "", "twenty ", "thirty ", "forty ", "fifty ", "sixty ", "seventy ", "eighty ", "ninety " ]
+function reader(n,l) {
 	if ( l == 1) {
-		onesDigit = n[0];
-		final = onesList[onesDigit];
+		onesDgt = n[0];
+		final = dgt[onesDgt];
+		if ( onesDgt == 0) {
+			final = "zero";
+		}
 	}
 	if (l == 2) {
-		onesDigit = n[1];
-		tensDigit = n[0];
-		final = tensList[tensDigit] + onesList[onesDigit];
-		if (tensDigit == 1) {
-			final = teensList[onesDigit];
+		onesDgt = n[1];
+		tensDgt = n[0];
+		final = tens[tensDgt] + dgt[onesDgt];
+		if (tensDgt == 1) {
+			final = tns[onesDgt];
 		}
 	}
 	if (l == 3) {
-		onesDigit = n[2];
-		tensDigit = n[1];
-		hundDigit = n[0];
-		if (hundDigit == 0) {
+		onesDgt = n[2];
+		tensDgt = n[1];
+		hundDgt = n[0];
+		if (hundDgt == 0) {
 			hund = "";
 		} else {
 			hund = " hundred ";
 		}
-		final = onesList[hundDigit] + hund + tensList[tensDigit] + onesList[onesDigit];
-		if (tensDigit == 1) {
-			final = onesList[hundDigit] + hund + teensList[onesDigit];
+		final = dgt[hundDgt] + hund + tens[tensDgt] + dgt[onesDgt];
+		if (tensDgt == 1) {
+			final = dgt[hundDgt] + hund + tns[onesDgt];
 		}
 	}
-	switch(n) {
-		case "0":
-			final = "zero";
-			break;
-		case "NaN":
-			final = "NaN/Not a number (input a number)";
-			break;
+	if (n == "NaN") {
+		final = "NaN/Not a number (input a number)";
 	}
 	return final;
 }
-/* Add functions to handle if "thousand/million" really
-should be placed (cuz zero's) */
-function handleThousand(t) {
-	if (t == 0) {
-		th =  "";
+function ext(v,type) {
+	if (v == 0) {
+		out =  "";
 	} else {
-		th = " thousand ";
+		out = type;
 	}
-	return th;
+	return out;
 }
-function handleMillion(m) {
-	if (m == 0) {
-		mil =  "";
-	} else {
-		mil = " million ";
-	}
-	return mil;
-}
-function handleBillion(b) {
-	if (b == 0) {
-		bil =  "";
-	} else {
-		bil = " billion ";
-	}
-	return bil;
-}
-function handleTrillion(tr) {
-	if (tr == 0) {
-		tri =  "";
-	} else {
-		tri = " trillion ";
-	}
-	return tri;
-}
-// Create a function that uses reader()
-function spellNumber(n) {
-	var n = document.getElementById("input").value;
-	var nInt = parseInt(n);
-	dec = (n % 1).toFixed(2) * 100 ;
-	dec = parseInt(Math.abs(dec)).toString();
+function spell(n) {
+	n = document.getElementById("input").value;
+	nInt = parseInt(n);
+	// isolate the the first 2 decimals only
+	dec = (n % 1).toFixed(2) * 100 ; dec = parseInt(Math.abs(dec)).toString();
 	// check if the number is negative.
-	ng = 0;
 	if ( nInt < 0) {
-		var nInt = Math.abs(nInt);
+		nInt = Math.abs(nInt);
 		ng = 1;
+	} else {
+		ng = 0;
 	}
-	var n = nInt.toString();
-	var l = n.length;
-	// Add cases that depend on l's length (to assign it's spelling and extension)
+	// convert n to integer then reconvert to string to remove stray zeroes
+	 n = nInt.toString();
+	 l = n.length;
 	switch(l) {
-		// cases that reader() can normally handle
-		case 1:
-		case 2:
-		case 3:
+		// cases reader() does normally
+		case 1: case 2: case 3:
 			output = reader(n,l);
 			break;
-		/* cases reader() cannot handle normally, so we split the numbers
-		by 3 and pass those on to reader() to output */
+		/* cases reader() can't do normally, so
+		we split the number by 3 and pass those */
 		case 4:
-			hun = n.substr(-3);
-			t = n.substr(0,1);
-			output = reader(t,1) + handleThousand(t) + reader(hun,3);
+			hun = n.substr(-3); t = n.substr(0,1);
+			output = reader(t,1) + ext(t," thousand ") + reader(hun,3);
 			break;
 		case 5:
-			hun = n.substr(-3);
-			t = n.substr(0,2);
-			output = reader(t,2) + handleThousand(t) + reader(hun,3);
+			hun = n.substr(-3); t = n.substr(0,2);
+			output = reader(t,2) + ext(t, " thousand ") + reader(hun,3);
 			break;
 		case 6:
-			hun = n.substr(-3);
-			t = n.substr(0,3);
-			output = reader(t,3) + handleThousand(t) + reader(hun,3);
+			hun = n.substr(-3); t = n.substr(0,3);
+			output = reader(t,3) + ext(t, " thousand ") + reader(hun,3);
 			break;
 		case 7:
-			hun = n.substr(-3);
-			t = n.substr(1,3);
-			m = n.substr(0,1);
-			output = reader(m,1) + handleMillion(m) + reader(t,3) +
-				handleThousand(t) + reader(hun,3);
+			hun = n.substr(-3); t = n.substr(1,3); m = n.substr(0,1);
+			output = reader(m,1) + ext(m," million ") + reader(t,3) + ext(t, " thousand ") + reader(hun,3);
 			break;
 		case 8:
-			hun = n.substr(-3);
-			t = n.substr(2,3);
-			m = n.substr(0,2);
-			output = reader(m,2) + handleMillion(m) + reader(t,3) +
-				handleThousand(t) + reader(hun,3);
+			hun = n.substr(-3); t = n.substr(2,3); m = n.substr(0,2);
+			output = reader(m,2) + ext(m," million ") + reader(t,3) +
+				ext(t, " thousand ") + reader(hun,3);
 			break;
 		case 9:
-			hun = n.substr(-3);
-			t = n.substr(3,3);
-			m = n.substr(0,3);
-			output = reader(m,3) + handleMillion(m) + reader(t,3) +
-				handleThousand(t) + reader(hun,3);
+			hun = n.substr(-3);	t = n.substr(3,3);m = n.substr(0,3);
+			output = reader(m,3) + ext(m," million ") + reader(t,3) + ext(t, " thousand ") + reader(hun,3);
 			break;
 		case 10:
-			hun = n.substr(-3);
-			t = n.substr(4,3);
-			m = n.substr(1,3);
-			b = n.substr(0,1);
-			output = reader(b,1) + handleBillion(b) + reader(m,3) + handleMillion(m) + reader(t,3) +
-				handleThousand(t) + reader(hun,3);
+			hun = n.substr(-3); t = n.substr(4,3); m = n.substr(1,3); b = n.substr(0,1);
+			output = reader(b,1) + ext(b," billion ") + reader(m,3) + ext(m," million ") + reader(t,3) + ext(t, " thousand ") + reader(hun,3);
 			break;
 		case 11:
-			hun = n.substr(-3);
-			t = n.substr(5,3);
-			m = n.substr(2,3);
-			b = n.substr(0,2);
-			output = reader(b,2) + handleBillion(b) + reader(m,3) + handleMillion(m) + reader(t,3) +
-				handleThousand(t) + reader(hun,3);
+			hun = n.substr(-3); t = n.substr(5,3); m = n.substr(2,3); b = n.substr(0,2);
+			output = reader(b,2) + ext(b," billion ") + reader(m,3) + ext(m," million ") + reader(t,3) +
+				ext(t, " thousand ") + reader(hun,3);
 			break;
 		case 12:
-			hun = n.substr(-3);
-			t = n.substr(6,3);
-			m = n.substr(3,3);
-			b = n.substr(0,3);
-			output = reader(b,3) + handleBillion(b) + reader(m,3) + handleMillion(m) + reader(t,3) +
-				handleThousand(t) + reader(hun,3);
+			hun = n.substr(-3); t = n.substr(6,3); m = n.substr(3,3); b = n.substr(0,3);
+			output = reader(b,3) + ext(b," billion ") + reader(m,3) + ext(b," million ") + reader(t,3) + ext(t," thousand ") + reader(hun,3);
 			break;
 		case 13:
-			hun = n.substr(-3);
-			t = n.substr(7,3);
-			m = n.substr(3,3);
-			b = n.substr(1,3);
-			tr = n.substr(0,1);
-			output = reader(tr,1) + handleTrillion(tr) + reader(b,3) + handleBillion(b) + reader(m,3) + handleMillion(m) + reader(t,3) +
-				handleThousand(t) + reader(hun,3);
+			hun = n.substr(-3); t = n.substr(7,3); m = n.substr(3,3); b = n.substr(1,3); tr = n.substr(0,1);
+			output = reader(tr,1) + ext(t," trillion ") + reader(b,3) + ext(b," billion ") + reader(m,3) + ext(b," million ") + reader(t,3) + ext(t," thousand ") + reader(hun,3);
 			break;
 		case 14:
-			hun = n.substr(-3);
-			t = n.substr(8,3);
-			m = n.substr(5,3);
-			b = n.substr(2,3);
-			tr = n.substr(0,2);
-			output = reader(tr,2) + handleTrillion(tr) + reader(b,3) + handleBillion(b) + reader(m,3) + handleMillion(m) + reader(t,3) +
-				handleThousand(t) + reader(hun,3);
+			hun = n.substr(-3); t = n.substr(8,3); 	m = n.substr(5,3);	b = n.substr(2,3); tr = n.substr(0,2);
+			output = reader(tr,2) + ext(t," trillion ") + reader(b,3) + ext(b," billion ") + reader(m,3) + ext(b," million ") + reader(t,3) + ext(t," thousand ") + reader(hun,3);
 			break;
 		case 15:
-			hun = n.substr(-3);
-			t = n.substr(9,3);
-			m = n.substr(6,3);
-			b = n.substr(3,3);
-			tr = n.substr(0,3);
-			output = reader(tr,3) + handleTrillion(tr) + reader(b,3) + handleBillion(b) + reader(m,3) + handleMillion(m) + reader(t,3) +
-				handleThousand(t) + reader(hun,3);
+			hun = n.substr(-3); t = n.substr(9,3); m = n.substr(6,3); b = n.substr(3,3); tr = n.substr(0,3);
+			output = reader(tr,3) + ext(t," trillion ") + reader(b,3) + ext(b," billion ") + reader(m,3) + ext(b," million ") + reader(t,3) + ext(t," thousand ") + reader(hun,3);
 			break;
 		default:
 			output = "unsupported length";
@@ -200,5 +130,5 @@ function spellNumber(n) {
 		decl = dec.length;
 		output = output + " and " + reader(dec,decl) + " hundredths";
 	}
-	document.getElementById("output").innerHTML = output; 
+	document.getElementById("output").innerHTML = output;
 }
