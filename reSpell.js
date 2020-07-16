07c0ca29a8ac4cb650297958471cd78194861369
MiKl-LS <51372994+MiKl-LS@users.noreply.github.com>
2020-07-16 12:18:04 +0800

    yay no more 'one thousand zero'
    also a lil code rewrite

--- reSpell.js
+++ reSpell.js
@@ -4,23 +4,21 @@ function reSpell(number) {
 	prefixArray = [ "", " thousand", " million", " billion", " trillion", " quadrillion", " quintillion", " sextillion", " septillion", " octillion", " nonillion", " decillion" ]
 	function reader(n) { // heart of reSpell.js
 		n = parseInt(n).toString(); l = n.length;
-		if ( l <= 2 && n < 20) { spelled = (onesArray[n]); }
-		if ( l == 2 && n > 19) { spelled = (tensArray[n.substr(0,1)] + onesArray[n.substr(1,1)]); }
-		if ( l == 3 && n[1] == 1 ) { spelled = (onesArray[n.substr(0,1)] + " hundred" + onesArray[n.substr(1,2)]); }
-		if ( l == 3 && n[1] != 1 ) { spelled = (onesArray[n.substr(0,1)] + " hundred" + tensArray[n.substr(1,1)] + onesArray[n.substr(2,1)]); }
-		return spelled.trim();
+		if ( l <= 2 && n < 20) { return (onesArray[n]).trim()s; }
+		if ( l == 2 && n > 19) { return (tensArray[n.substr(0,1)] + onesArray[n.substr(1,1)]).trim(); }
+		if ( l == 3 && n[1] == 1 ) { return (onesArray[n.substr(0,1)] + " hundred" + onesArray[n.substr(1,2)]).trim(); }
+		if ( l == 3 && n[1] != 1 ) { return (onesArray[n.substr(0,1)] + " hundred" + tensArray[n.substr(1,1)] + onesArray[n.substr(2,1)]).trim(); }
 	}
-	if (number == "0") { return "zero"; } // Wont work when integrated with reader() so it's here instead
+	if ( number[0] == "0" || number[0] == 0) { return "zero"; } 
 	n = number.toString().replace(/^0+/, ''); l = n.length; // truncate the leading zeroes
+	if ( l > 36) { return "Unsupported Length: " + l; } // prevent overflow lol
 	d  = false; ng = false; // values so JS doesnt scream 'Undefined'
 	if (n.match(/-/g) != null) { n = n.replace(/-/g,""); ng = true;	}
 	if (n.indexOf(".") != -1) { d = (n % 1).toFixed(2) * 100; d = parseInt(Math.abs(d)).toString(); n = n.substr("0",n.indexOf(".")); }
 	if ( l < 3) { output = reader(n);
 	} else {
-		if ( l > 36) { return "ERROR: Unsupported Length"; }
-		modulus = l % 3; // modify the n to be divisible by 3
-		if (modulus == 1) { n = "00" + n; }
-		if (modulus == 2) { n = "0" + n; }
+		modulus = l % 3; 
+		if (modulus == 1) { n = "00" + n; } if (modulus == 2) { n = "0" + n; } // modify the n to be divisible by 3
 		l = n.length; // redefine l after modifying
 		output = []; currentValue = []; currentDecimal = []
 		for ( i = 0, ind = 0; i <  l; i += 3, ind++) { // store the new number in an array by 3
