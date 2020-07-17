0aadfc148c1d441bbee1bfd621476c9c5fba84d1
MiKl-LS <51372994+MiKl-LS@users.noreply.github.com>
2020-07-17 18:18:45 +0800

    I got bored: Update
    - Supports up to 66+ digits (hundred vigintillion) 
    - Boring changes...

--- reSpell.js
+++ reSpell.js
@@ -1,7 +1,7 @@
 function reSpell(number) {
 	onesArray = [ "", " one", " two", " three", " four", " five", " six", " seven", " eight", " nine", " ten", " eleven", " twelve", " thirteen", " fourteen", " fifteen", " sixteen", " seventeen", " eighteen", " nineteen" ]
 	tensArray = [ "", "", " twenty", " thirty", " forty", " fifty", " sixty", " seventy", " eighty", " ninety" ]
-	prefixArray = [ "", " thousand", " million", " billion", " trillion", " quadrillion", " quintillion", " sextillion", " septillion", " octillion", " nonillion", " decillion" ]
+	suffixArray = [ "", " thousand", " million", " billion", " trillion", " quadrillion", " quintillion", " sextillion", " septillion", " octillion", " nonillion", " decillion", " undecillion", " duodecillion", " tredecillion", " quattuordecillion", " quindecillion", " sexdecillion", " septendecillion", " octodecillion", " novemdecillion", " vigintillion" ]
 	function reader(n) { // heart of reSpell.js
 		n = parseInt(n).toString(); l = n.length; // makes sure '012' is read as '12'
 		if ( n == "0") { return "zero"; }
@@ -10,24 +10,22 @@ function reSpell(number) {
 		if ( l == 3 && n[1] == 1 ) { return (onesArray[n.substr(0,1)] + " hundred" + onesArray[n.substr(1,2)]).trim(); }
 		if ( l == 3 && n[1] != 1 ) { return (onesArray[n.substr(0,1)] + " hundred" + tensArray[n.substr(1,1)] + onesArray[n.substr(2,1)]).trim(); }
 	}
-	n = number.toString().replace(/\b0+/g, ""); l = n.length; // truncate the leading zeroes
-	if (n == "") { n = "0"; }
-	if ( l > 36) { return "Unsupported Length: " + l; } // prevent overflow lol
+	if ( number.length > 66) { return "Unsupported Length: " + number.length; } // prevent overflow lol
+	n = number.toString().replace(/\b0+/g, ''); if (n == "") { n = "0"; } // truncate the leading zeroes
 	d  = false; ng = false; // values so JS doesnt scream 'Undefined'
 	if (n.match(/-/g) != null) { n = n.replace(/-/g,""); ng = true;	}
 	if (n.indexOf(".") != -1) { d = Number(n.substr(n.indexOf("."))).toFixed(2).substr(2); n = n.substr("0",n.indexOf(".")); }
-	if ( l < 3) { output = reader(n); } else {
+	l = n.length; 
+	if ( l <= 3) { output = reader(n); /* default */ } else {
 		modulus = l % 3; 
 		if (modulus == 1) { n = "00" + n; } if (modulus == 2) { n = "0" + n; } // modify the n to be divisible by 3
 		l = n.length; // redefine l after modifying
 		output = []; currentValue = []; currentDecimal = []
-		for ( i = 0, ind = 0; i <  l; i += 3, ind++) { // store the new number in an array by 3
-			currentValue[ind] = parseInt(n.substr(i,3));
-		}
+		for ( i = 0, ind = 0; i <  l; i += 3, ind++) { currentValue[ind] = parseInt(n.substr(i,3)); } // store the new number in an array by 3
 		for (currentValue.reverse(), i = 0; i < currentValue.length; i++) {
 			val = reader(currentValue[i].toString());
 			if (val == "" || currentValue[i] == 0) { continue; } else { // do nothing (removes useless values)
-				output.push(val + prefixArray[i]);
+				output.push(val + suffixArray[i]);
 			}
 		}
 		output = output.reverse().toString().replace(/,/g," "); // reverse the reversed output & remove the commas
