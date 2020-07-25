fa8911ce064397ac1493225647331d4e065b45e3
MiKl-LS <51372994+MiKl-LS@users.noreply.github.com>
2020-07-25 09:12:11 +0800

    wow compression

--- reSpell.js
+++ reSpell.js
@@ -1,35 +1,31 @@
-function reSpell(number) {
-	onesArray = [ "", " one", " two", " three", " four", " five", " six", " seven", " eight", " nine", " ten", " eleven", " twelve", " thirteen", " fourteen", " fifteen", " sixteen", " seventeen", " eighteen", " nineteen" ]
-	tensArray = [ "", "", " twenty", " thirty", " forty", " fifty", " sixty", " seventy", " eighty", " ninety" ]
-	suffixArray = [ "", " thousand", " million", " billion", " trillion", " quadrillion", " quintillion", " sextillion", " septillion", " octillion", " nonillion", " decillion", " undecillion", " duodecillion", " tredecillion", " quattuordecillion", " quindecillion", " sexdecillion", " septendecillion", " octodecillion", " novemdecillion", " vigintillion" ]
-	function reader(n) { // heart of reSpell.js
-		n = parseInt(n).toString(); l = n.length; // makes sure '012' is read as '12'
-		if ( n == "0") { return "zero"; }
-		if ( l <= 2 && n < 20) { return (onesArray[n]).trim(); }
-		if ( l == 2 && n > 19) { return (tensArray[n.substr(0,1)] + onesArray[n.substr(1,1)]).trim(); }
-		if ( l == 3 && n[1] == 1 ) { return (onesArray[n.substr(0,1)] + " hundred" + onesArray[n.substr(1,2)]).trim(); }
-		if ( l == 3 && n[1] != 1 ) { return (onesArray[n.substr(0,1)] + " hundred" + tensArray[n.substr(1,1)] + onesArray[n.substr(2,1)]).trim(); }
+function reSpell(value) {
+	onesA = [ "", " one", " two", " three", " four", " five", " six", " seven", " eight", " nine", " ten", " eleven", " twelve", " thirteen", " fourteen", " fifteen", " sixteen", " seventeen", " eighteen", " nineteen"] 
+	tensA = [ "", "", " twenty", " thirty", " forty", " fifty", " sixty", " seventy", " eighty", " ninety" ] 
+	suffixA = ["", " thousand", " million", " billion", " trillion", " quadrillion", " quintillion", " sextillion", " septillion", " octillion", " nonillion", " decillion", " undecillion", " duodecillion", " tredecillion", " quattuordecillion", " quindecillion", " sexdecillion", " septendecillion", " octodecillion", " novemdecillion", " vigintillion" ]
+	n = value.toString().replace(/\b(0(?!\b))+/g, "");
+	d  = false; ng = false; // default values so JS doesnt scream 'Undefined'
+	if (n.match(/-/g) != null) { n = n.replace(/-/g,""); ng = true;	} if (n.indexOf(".") != -1) { d = Number(n.substr(n.indexOf("."))).toFixed(2).substr(2); n = n.substr("0",n.indexOf(".")); }
+	function parser(value) {
+		n = parseInt(value).toString();
+		if (n == "0" && n.length == 1) { return "zero"; } 
+			else if (n.length <= 2 && n < 20) { return onesA[n].trim(); }
+				else if (n.length <= 2 && n > 19){ return (tensA[n.substr(0,1)] + onesA[n.substr(1,1)]).trim(); }
+		if (n.length == 3 && n.substr(1,2) < 20) { return (onesA[n.substr(0,1)] + " hundred" + onesA[parseInt(n.substr(1,2))]).trim(); }
+			else if (n.length == 3 && n.substr(1,2) > 19) { return (onesA[n.substr(0,1)] + " hundred" + tensA[n.substr(1,1)] + onesA[n.substr(2,1)]).trim(); }
 	}
-	if ( number.length > 66) { return "Unsupported Length: " + number.length; } // prevent overflow lol
-	n = number.toString().replace(/\b(0(?!\b))+/g, "") // truncate the leading zeroes
-	d  = false; ng = false; // values so JS doesnt scream 'Undefined'
-	if (n.match(/-/g) != null) { n = n.replace(/-/g,""); ng = true;	}
-	if (n.indexOf(".") != -1) { d = Number(n.substr(n.indexOf("."))).toFixed(2).substr(2); n = n.substr("0",n.indexOf(".")); }
-	l = n.length; 
-	if ( l <= 3) { output = reader(n); /* default */ } else {
-		modulus = l % 3; 
-		if (modulus == 1) { n = "00" + n; } if (modulus == 2) { n = "0" + n; } // modify the n to be divisible by 3
-		l = n.length; // redefine l after modifying
-		output = []; currentValue = []; currentDecimal = []
-		for ( i = 0, ind = 0; i <  l; i += 3, ind++) { currentValue[ind] = parseInt(n.substr(i,3)); } // store the new number in an array by 3
-		for (currentValue.reverse(), i = 0; i < currentValue.length; i++) {
-			val = reader(currentValue[i].toString());
-			if (val == "" || currentValue[i] == 0) { continue; } else { // do nothing (removes useless values)
-				output.push(val + suffixArray[i]);
+	if (n.length >= 66) { return "Value too large " + n.length; } if (n.match('[a-zA-Z]') != null || n == "") { return "NaN (Not a Number)"; }
+	if (n.length <= 3) { s = parser(n); } // from '0' through '999'
+			else { spelledV = []; currentV = [];
+				m = n.length % 3; 
+				if (m == 1) { n = "00" + n; } else if (m == 2) { n = "0" + n; }
+				for ( i = 0, g = 0; i < n.length; i += 3, g++) { currentV[g] = parseInt(n.substr(i,3)); }
+				for (currentV.reverse(), i = 0; i < currentV.length; i++) {
+					v = parser(currentV[i].toString());
+					if (v == "" || currentV[i] == 0) { continue; } 
+						else { spelledV.push(v + suffixA[i]); }
+				}
+				s = spelledV.reverse().toString().replace(/,/g," ");
 			}
-		}
-		output = output.reverse().toString().replace(/,/g," "); // reverse the reversed output & remove the commas
-	}
-	if (ng != false) { output = "negative " + output;} if (d != false) { output += " and " + reader(d) + " hundredths"; }
-	return output; 
+	if (ng != false) { s = "negative " + s;} if (d != false) { s += " and " + parser(d) + " hundredths"; }
+	return s;
 }
